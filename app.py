1d9d697404fa634c1a8a9b4d20931cda7198ff06
Irina <irina.redactor@gmail.com>
2025-05-13 17:23:00 +0100

    added all_spaces route, modified space model and repo to include img url, designed all_spaces template

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask_login import LoginManager, login_user
 from lib.database_connection import get_flask_database_connection
 from lib.user import User
 from lib.user_repository import UserRepository
+from lib.space_repository import SpaceRepository
 
 # Create a new Flask app
 app = Flask(__name__, static_folder='static')
@@ -11,9 +12,6 @@ app.secret_key = os.urandom(24)
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = "login"
-
-users = {"user_1": {"username": "user_1", "password": "password_1"},
-        "user_2" :{"username": "user_2", "password": "password_2"}}
 
 # == Your Routes Here ==
 
@@ -27,26 +25,38 @@ def get_index():
 
 @app.route('/sign_in', methods=['GET','POST'])
 def get_login():
-    conn = get_flask_database_connection()
+    conn = get_flask_database_connection(app)
     repo = UserRepository(conn)
+    
 
     if request.method == "POST":
         username = request.form['username']
         password = request.form['password']
-        active_user = repo.get_user_by_username(username)
-        if active_user.password == password:
-            login_user(active_user)
-            return render_template('index.html')
+        if repo.user_exists(username): 
+            active_user = repo.get_user_by_username(username)
+            if active_user.password == password:
+                login_user(active_user)
+                return render_template('index.html')
+            else:
+                flash('Invalid Credentials')
         else:
             flash('Invalid Credentials')
     return render_template('sign_in.html')
 
+@app.route('/all_spaces', methods=['GET'])
+def get_all_spaces():
+    conn = get_flask_database_connection(app)
+    repo = SpaceRepository(conn)
+    spaces = repo.get_all()
+    return render_template('all_spaces.html', spaces=spaces)
 
 @login_manager.user_loader
 def load_user(user_id):
-    if user_id in users:
-        user_data = users[user_id]
-        return User(user_id, user_data["name"])
+    conn = get_flask_database_connection(app)
+    repo = UserRepository(conn)
+    user = repo.get_user_by_id(user_id)
+    if user:
+        return user
     return None
 
 # These lines start the server if you run this file directly
--- lib/space_repository.py
+++ lib/space_repository.py
@@ -7,9 +7,9 @@ class SpaceRepository:
 
     def get_all(self):
         rows = self._connection.execute('SELECT * FROM spaces')
-        return [Space(row['id'], row['name'], row['description'], row['price_per_night'], row['owner_id']) for row in rows]
+        return [Space(row['id'], row['name'], row['description'], row['price_per_night'], row['owner_id'], row['url']) for row in rows]
     
     def get_single_space(self, space_id):
         rows = self._connection.execute('SELECT * FROM spaces WHERE id = %s', [space_id])
         row = rows[0]
-        return Space(row['id'], row['name'], row['description'], row['price_per_night'], row['owner_id'])
+        return Space(row['id'], row['name'], row['description'], row['price_per_night'], row['owner_id'], row['url'])
