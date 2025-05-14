2539e7793a94eaf6b5d39f12a60c23fc79c1402b
Irina <irina.redactor@gmail.com>
2025-05-14 14:49:43 +0100

    added create_user method and sign up route

--- app.py
+++ app.py
@@ -8,25 +8,18 @@ from lib.space_repository import SpaceRepository
 from lib.space import Space
 
 
-# Create a new Flask app
 app = Flask(__name__, static_folder='static')
 app.secret_key = os.urandom(24)
 login_manager = LoginManager()
 login_manager.init_app(app)
-login_manager.login_view = "login"
+login_manager.login_view = "sign_in"
 
-# == Your Routes Here ==
-
-# GET /index
-# Returns the homepage
-# Try it:
-#   ; open http://localhost:5001/index
 @app.route('/index', methods=['GET'])
 def get_index():
     return render_template('index.html')
 
 @app.route('/sign_in', methods=['GET','POST'])
-def get_login():
+def sign_in():
     conn = get_flask_database_connection(app)
     repo = UserRepository(conn)
     
@@ -53,12 +46,12 @@ def get_all_spaces():
     return render_template('all_spaces.html', spaces=spaces)
 
 @app.route('/create_space', methods=['GET'])
-# @login_required
+@login_required
 def create_space():
     return render_template('create_space.html')
 
 @app.route('/create_space', methods=["POST"])
-# @login_required
+@login_required
 def create_space_post():
     conn = get_flask_database_connection(app)
     repository = SpaceRepository(conn)
@@ -83,7 +76,7 @@ def create_space_post():
 
 
 @app.route('/space/<id>', methods=['GET'])
-# @login_required
+@login_required
 def get_single_space(id):
     conn = get_flask_database_connection(app)
     repository = SpaceRepository(conn)
@@ -91,7 +84,18 @@ def get_single_space(id):
     space_data = repository.get_single_space(id)
     return render_template('/single_space.html', space = space_data)
 
+@app.route('/sign_up', methods=['GET', 'POST'])
+def sign_up():
+    conn = get_flask_database_connection(app)
+    repo = UserRepository(conn)
     
+    if request.method == "POST":
+        new_user = User(None, request.form['username'], request.form['email'], request.form['password'], request.form['phone_number'])
+        active_user_id = repo.create_user(new_user)
+        login_user(active_user_id)
+        return redirect('/all_spaces')
+    else:
+        return render_template('/sign_up.html')
 
 
 @login_manager.user_loader
@@ -103,8 +107,7 @@ def load_user(user_id):
         return user
     return None
 
-# These lines start the server if you run this file directly
-# They also start the server configured to use the test database
-# if started in test mode.
+
+
 if __name__ == '__main__':
     app.run(debug=True, port=int(os.environ.get('PORT', 5001)))
